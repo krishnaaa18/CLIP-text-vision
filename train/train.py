da91fb0f7d0614ac6fd9df42a807259221ad7409
krishnaaa18 <kkbbhardwaj18@gmail.com>
2026-02-20 11:36:47 +0530

    20/02/26

--- train/train.py
+++ train/train.py
@@ -31,15 +31,56 @@ def train():
     model = CLIPModel(embed_dim=256).to(device)
     loss_fn = CLIPLoss()
 
-    optimizer = AdamW(model.parameters(), lr=1e-4)
+    # -------------------------
+    # Phase 1: Freeze Backbones
+    # -------------------------
 
-    epochs = 10
+    for param in model.image_encoder.backbone.parameters():
+        param.requires_grad = False
+
+    for param in model.text_encoder.backbone.parameters():
+        param.requires_grad = False
+
+    # Train projection heads + temperature only
+    optimizer = AdamW(
+        list(model.image_encoder.projection.parameters()) +
+        list(model.text_encoder.projection.parameters()) +
+        [model.log_temperature],
+        lr=1e-4
+    )
+
+    epochs = 15
 
     for epoch in range(epochs):
+
+        # -------------------------
+        # Phase 2: Unfreeze at Epoch 4
+        # -------------------------
+        if epoch == 3:
+            print("\nUnfreezing top layers...\n")
+
+            # Unfreeze last ResNet block
+            for param in model.image_encoder.backbone.layer4.parameters():
+                param.requires_grad = True
+
+            # Unfreeze last 2 transformer layers
+            for param in model.text_encoder.backbone.transformer.layer[-2:].parameters():
+                param.requires_grad = True
+
+            # New optimizer with differential learning rates
+            optimizer = AdamW([
+                {"params": model.image_encoder.backbone.layer4.parameters(), "lr": 1e-5},
+                {"params": model.text_encoder.backbone.transformer.layer[-2:].parameters(), "lr": 1e-5},
+                {"params": model.image_encoder.projection.parameters(), "lr": 1e-4},
+                {"params": model.text_encoder.projection.parameters(), "lr": 1e-4},
+                {"params": [model.log_temperature], "lr": 1e-4}
+            ])
+
         model.train()
         total_loss = 0
 
         for batch in tqdm(loader):
+
             images = batch["image"].to(device)
             input_ids = batch["input_ids"].to(device)
             attention_mask = batch["attention_mask"].to(device)
@@ -50,16 +91,19 @@ def train():
 
             optimizer.zero_grad()
             loss.backward()
-            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+
+            # Gradient clipping for stability
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+
             optimizer.step()
 
             total_loss += loss.item()
 
         avg_loss = total_loss / len(loader)
-
         print(f"Epoch [{epoch+1}/{epochs}] Loss: {avg_loss:.4f}")
+
     torch.save(model.state_dict(), "clip_model.pth")
-    print("Model saved as clip_model.pth")
+    print("\nModel saved as clip_model.pth\n")
 
 
 if __name__ == "__main__":
